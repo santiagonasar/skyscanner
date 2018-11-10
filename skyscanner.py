0523c219dd14ecbd2e82c2ddff8836566d15f7c5
santiagonasar42 <mat.p.wasilewski@gmail.com>
2018-11-10 21:49:02 +0000

    Class added. Intended to correspond to one observed route.

--- skyscanner.py
+++ skyscanner.py
@@ -1,3 +1,11 @@
 import json
+import requests
+import dataPull as dp
 
-print("Hello World!")
+# URL to download json data from API
+url = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/GB/eur/en-GB/uk/pl/anytime/anytime?apikey=ha626660336299059327728735244347'
+
+data = dp.get(url)
+
+class route():
+    One two threeg
