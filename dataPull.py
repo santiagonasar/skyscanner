312de7fe2c40ba0adc5e7497aa38628f7d704a40
santiagonasar42 <mat.p.wasilewski@gmail.com>
2018-11-10 22:33:22 +0000

    Parameters to be passed adjusted, names changed. No return flight will be included.

--- dataPull.py
+++ dataPull.py
@@ -6,13 +6,19 @@ import json
 # url = 'http://partners.api.skyscanner.net/apiservices/geo/v1.0?apikey=ha626660336299059327728735244347'
 
 # takes in url and returns dictionary pulled from API
-class one:
-    def __init__(self, market, outbound, destination, direct=false, currency):
+class browse:
+    def __init__(self, market, currency, outbound, inbound,
+                 dateStart, dateStop, direct=false, apikey):
+        # /GB/eur/en-GB/uk/pl/anytime/anytime?apikey=ha626660336299059327728735244347'
         self.market = market
         self.outbound = outbound
-        self.destination = destination
+        self.inbound = inbound
         self.direct = direct
         self.currency = currency
+        self.apikey = apikey
+        self.dateStart = dateStart
+        self.dateStop = dateStop
+        self.dateReturn = ''
 
     # Function returns browseQuotes query result
     def getQuotes(url):
@@ -20,7 +26,21 @@ class one:
         pulledDict = pulledData.json()
         return pulledDict
 
+
     # Function prepares URL to pass to getQuotes
     # Uses variables passed to class to determine which parameters are relevant
     # Has to return url (query) which will return proper record
     def prepareURL(self):
+        # Sample URL part two
+        # /GB/eur/en-GB/uk/pl/anytime/anytime?apikey=ha626660336299059327728735244347'
+
+        url = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0'
+        url += '/' + self.market
+        url += '/' + self.currency
+        url += '/en-GB'
+        url += '/' + self.outbound
+        url += '/' + self.inbound
+        url += '/' + outboundDate
+        url += '/'
+        url += '?apikey=' + self.apikey
+        return url
--- skyscanner.py
+++ skyscanner.py
@@ -1,11 +1,26 @@
 import json
 import requests
-import dataPull as dp
+from dataPull import browse
 
 # URL to download json data from API
 url = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/GB/eur/en-GB/uk/pl/anytime/anytime?apikey=ha626660336299059327728735244347'
 
-data = dp.get(url)
+data = browse.getQuotes(url)
 
 class route():
-    One two threeg
+    # Initialization of route.
+    # Parameters must be passed to the class to specify, which parameters are to be observed
+    # And which apikey should be used.
+    # Initialization will later be passed to browse class in dataPull, as they are pretty
+    def __init__(self, market, currency, outbound, inbound,
+                 dateStart, dateStop, direct=false, apikey):
+        # /GB/eur/en-GB/uk/pl/anytime/anytime?apikey=ha626660336299059327728735244347'
+        self.market = market
+        self.outbound = outbound
+        self.inbound = inbound
+        self.direct = direct
+        self.currency = currency
+        self.apikey = apikey
+        self.dateStart = dateStart
+        self.dateStop = dateStop
+        self.dateReturn = ''
